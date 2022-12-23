53277765480677bb2ccae6b054c2f055472d023f
francop <franco.pradelli94@gmail.com>
2022-12-23 11:32:01 +0100

    Improved documentation

--- README.md
+++ README.md
@@ -10,8 +10,8 @@ The materials contained in this folder allow the reproduction of the simulations
 and angiogenesis with FEniCS and Mocafe.
 
 ## Quick instructions
-Modify the `main.py` script according to your needs to run a simulation. To ensure full reproducibility, we recommend 
-to use [Singularity](https://github.com/sylabs/singularity):
+The full code to reproduce our results and to test our implementation is contained in  `main.py`. 
+To ensure full reproducibility, we recommend to use [Singularity](https://github.com/sylabs/singularity):
 
 ```shell
 # pull container
@@ -50,6 +50,8 @@ srun singularity exec sif/mocafe_v1-5-0.sif python3 main.py -slurm_job_id $SLURM
 ex=$?
 ```
 
+You can change the script according to your needs.
+
 ## Simulation output
 The result of a simulation is a folder with the following structure:
 ```shell
@@ -68,7 +70,7 @@ The result of a simulation is a folder with the following structure:
 ├── tipcells.h5
 └── tipcells.xdmf
 ```
-Where the `*.xdmf` and `*.h5` files are the actual simulation output. More precisely:
+Where the `*.xdmf` and `*.h5` files are the actual simulation outputs. More precisely:
 
 | Files                            | Content                                       |
 |----------------------------------|-----------------------------------------------|
@@ -80,10 +82,10 @@ Where the `*.xdmf` and `*.h5` files are the actual simulation output. More preci
 
 You can load these file in ParaView to visualize them. [^1]
 
-There are also three folders:
+There are also three additional folders:
 - `0_reproduce` contains a copy of the script used to generate the simulation. 
 Useful to check the exact code used to generate the results.
-- `resume` contains the data to resume the simulation. See `main.py` for an example.
+- `resume` contains the data to resume the simulation (i.e. start a new simulation using the end of another simulation as initial condition). See `main.py` for an example.
 - `sim_info` contains:
   - `incremental_tipcells.json`: data for every tip cell at each time step.
   - `mesh_parameters.csv`: info regarding the mesh used for the simulation.
@@ -93,7 +95,7 @@ Useful to check the exact code used to generate the results.
 ## GitHub and Zenodo [![DOI](https://zenodo.org/badge/DOI/10.5281/zenodo.7330072.svg)](https://doi.org/10.5281/zenodo.7330072)
 
 This repository is available both on GitHub or on Zenodo. The GitHub version is meant to clone and reproduce the 
-results. On the Zenodo repository, you can access some of the generated output of the code, and the simulation 
+results. On the Zenodo repository, you can access to the generated outputs of the code, and the simulation 
 results that we used in our manuscript. See the section "Repository folders" (below) for additional info.
 
 ## Structure of this repository
@@ -113,7 +115,7 @@ This repository contains several folders:
 Let's see them one by one: 
 
 ### :file_folder: `input_images`
-Contains pateint-specific images, derived from the Optical Coherence Tomography Angiography. 
+Contains patient-specific images, derived from the Optical Coherence Tomography Angiography. 
 
 They are used in the program to generate the initial condition for the simulation.
 
@@ -141,7 +143,7 @@ Also, you can consider using the visualization script provided in `visualization
 On Zenodo, contains the Mocafe Singularity container used to run the simulations. It is recommended to use the same container
 to ensure reproducibility.
 
-To access the same container, just pull it from the cloud library:
+The recommended way to access the same container is to pull it from the cloud library:
 
 ```shell
 singularity build sif/mocafe_v1-5-0.sif library://fpradelli94/mocafe/mocafe:1.5.0
@@ -151,7 +153,7 @@ singularity build sif/mocafe_v1-5-0.sif library://fpradelli94/mocafe/mocafe:1.5.
 Contains the source code used in the `main.py` script. 
 
 ### :file_folder: `visualization`
-Contains scripts and data related to results visualization. There are two sub-folders: `paraview` and `python`
+Contains scripts and data related to the results' visualization. There are two sub-folders: `paraview` and `python`
 
 1. `paraview`
 
@@ -159,7 +161,7 @@ Contains a script, `section.py`, to visualize a simulation in 3D with [ParaView]
 It is the same used to generate the thumbnail of this `README`. It only works with 3D simulations and with ParaView >5.9.0.
 
 To use the script, open ParaView and, on the View tab, select "Python Shell". 
-A new window will appear with a botton "Run Script". Press the button and select the file `setion.py`.
+A new window will appear with a button "Run Script". Press the button and select the file `setion.py`.
 
 If the script runs correctly, it will ask for two inputs: the absolute paths of the simulation's `c.xdmf` file 
 and of the `phi.xdmf` file. Copy and paste them in the box that will appear and let ParaView handle the rest.
@@ -172,7 +174,7 @@ Contains the scripts used to produce some plots reported in the manuscript. More
 
 - `activation_tiles.py`, which generates a tiles plot of the parameters leading to angiognesis (Fig. 3 of the manuscript)
 using the table `sim_index.csv`.
-- `tipcells_in_time.py`, which generates a plot of the number of tipcells at any simulation step (like in Fig. 4 of the 
+- `tipcells_in_time.py`, which generates a plot of the number of tip cells at any simulation step (like in Fig. 4 of the 
 manuscript).
 
 ## Meta
@@ -186,4 +188,4 @@ To view a copy of this license, visit http://creativecommons.org/licenses/by-nc/
 
 [^1]: Don't know how? See examples in the [Mocafe Demo gallery](https://biocomputingup.github.io/mocafe/build/html/demo_doc/angiogenesis_3d.html#visualize-the-result-with-paraview)
 
-[^2]: For an example, see [Mocafe Demo gallery](https://biocomputingup.github.io/mocafe/build/html/demo_doc/multiple_pc_simulations.html#sphx-glr-demo-doc-multiple-pc-simulations-py))
+[^2]: For an example, see [Mocafe Demo gallery](https://biocomputingup.github.io/mocafe/build/html/demo_doc/multiple_pc_simulations.html#sphx-glr-demo-doc-multiple-pc-simulations-py)
--- main.py
+++ main.py
@@ -13,8 +13,12 @@ rank = comm_world.Get_rank()
 fenics.set_log_level(fenics.LogLevel.ERROR)
 
 
-def main():
+def tutorial_2d():
+    """
+    In this function, we provide some usage examples of the code to simulate our patient-specific RH.
 
+    All the examples here are in 2D, to make the computational effort affordable for any PC.
+    """
     # -- Generate simulation parameters from notebook
     parameters_nb = "notebooks/parameters.ipynb"
     if rank == 0:
@@ -51,7 +55,7 @@ def main():
 
     # Ex. 2: change some parameters value
     sim_parameters = Parameters(standard_parameters_df)
-    sim_parameters.set_value("min_tumor_diameter",                     # change initial tumor diameter
+    sim_parameters.set_value("min_tumor_diameter",                     # change initial tumor diameter (d_0)
                              sim_parameters.get_value("max_tumor_diameter"))
     run_simulation(spatial_dimension=spatial_dimension,
                    sim_parameters=sim_parameters,
@@ -74,6 +78,144 @@ def main():
     test_tip_cell_activation(spatial_dimension,
                              df_standard_params=standard_parameters_df,
                              out_folder_name="test_tc_activation")
+
+
+def reproduce_results():
+    """
+    This function contains the code to reproduce the simulations discussed in the manuscript (Fig 2, Fig 3, and Fig 4).
+
+    All these simulation are in 3D and have been executed on a powerful cluster (192 GB total RAM, 6 nodes, 96 cores).
+    """
+    # ---------------------------------------------------------------------------------------------------------------- #
+    # PREAMBLE
+    # Below, we generate the simulation parameters form the notebook, and we get the slurm job ID, if any.
+    # ---------------------------------------------------------------------------------------------------------------- #
+
+    # execute the notebook
+    parameters_nb = "notebooks/parameters.ipynb"
+    if rank == 0:
+        subprocess.run(["jupyter", "nbconvert", "--to", "html", "--execute", parameters_nb])
+
+    # load parameters as generated from the notebook
+    parameters_csv = "notebooks/out/g_parameters.csv"
+    standard_parameters_df = pd.read_csv(parameters_csv, index_col="name")
+
+    # get slurm job id, if any
+    if "-slurm_job_id" in sys.argv:
+        try:
+            slurm_job_id = int(sys.argv[sys.argv.index("-slurm_job_id") + 1])
+        except IndexError:
+            raise IndexError("slurm_job_id not specified. Found -slurm_job_id not followed by an actual id.")
+    else:
+        slurm_job_id = None
+
+    # wait for all processes
+    comm_world.Barrier()
+
+    # ---------------------------------------------------------------------------------------------------------------- #
+    # CODE FOR SIMULATION IN FIG 2
+    # Below, you can find the code to reproduce simulation in Fig.2. In the first part we configure the simulation,
+    # setting the correct values for the parameters. Then, we run the simulation using the appropriate function.
+    # ---------------------------------------------------------------------------------------------------------------- #
+
+    # load parameters
+    sim_parameters = Parameters(standard_parameters_df)
+
+    # change initial tumor diameter
+    sim_parameters.set_value("min_tumor_diameter", 0.5)  # correspond to d_0 (unit of measure is [sau])
+
+    # run simulation
+    run_simulation(spatial_dimension=3,
+                   sim_parameters=sim_parameters,
+                   steps=200,
+                   save_rate=10,
+                   out_folder_name="Fig2",
+                   sim_rationale=f"Reproducing simulation in Fig 2",
+                   slurm_job_id=slurm_job_id)
+
+    # ---------------------------------------------------------------------------------------------------------------- #
+    # CODE FOR SIMULATIONS IN FIG 3
+    # Below, you can find the code to reproduce simulations in Fig.3, where we tested tip cell activation with 500
+    # one-step simulations. To do so, there is a single function that handles everything. For additional details,
+    # you can check the implementation.
+    # The output is a csv file saved in saved_sim/Fig3/tipcell_activation.csv. From that, we generated Fig 3 using the
+    # script in ./rh_mocafe/visualization/python/tiles_plot.py
+    # ---------------------------------------------------------------------------------------------------------------- #
+
+    # test tip cell activation
+    test_tip_cell_activation(spatial_dimension=3,
+                             df_standard_params=standard_parameters_df,
+                             out_folder_name="Fig3")
+
+    # ---------------------------------------------------------------------------------------------------------------- #
+    # CODE FOR SIMULATIONS IN FIG 4a, 4b, 4c
+    # Below, you can find the code to reproduce simulations in Fig.4a, 4b, and 4c. In the first part we configure the
+    # simulation, setting the correct values for the parameters. Notice that we don't set V_pT, since it is already set
+    # in the Jupyter Notebook.
+    # ---------------------------------------------------------------------------------------------------------------- #
+
+    # load sim parameters
+    sim_parameters = Parameters(standard_parameters_df)
+
+    # set steps number
+    steps = 300
+
+    # compute initial tumor diameter, knowing that the final correspond to 600 um
+    td_f = sim_parameters.get_value("max_tumor_diameter")  # correspond to 600 um
+    tgr = sim_parameters.get_value("tgr")
+    td_i = td_f / (tgr ** (steps / 3))
+    sim_parameters.set_value("min_tumor_diameter", td_i)  # correspond to d_0
+
+    # test for different uptake values
+    V_uc_af_range = [21.1608695473508, 3.89679868445612, 0.7176]  # values given in [1 / tau]
+
+    # simulate
+    for sim_folder, V_uc_af_value in zip(["Fig4a", "Fig4b", "Fig4c"], V_uc_af_range):
+        # update V_uc_value
+        sim_parameters.set_value("V_uc_af", V_uc_af_value)
+
+        run_simulation(spatial_dimension=3,
+                       sim_parameters=sim_parameters,
+                       steps=steps,
+                       save_rate=10,
+                       out_folder_name=sim_folder,
+                       sim_rationale=f"Reproducing simulation in {sim_folder}",
+                       slurm_job_id=slurm_job_id)
+
+    # ---------------------------------------------------------------------------------------------------------------- #
+    # CODE FOR SIMULATIONS IN FIG 4d
+    # Below, you can find the code to reproduce simulations in Fig.4d. In the first part we configure the
+    # simulation, setting the correct values for the parameters. Notice that we don't set V_pT, since it is already set
+    # in the Jupyter Notebook.
+    # ---------------------------------------------------------------------------------------------------------------- #
+
+    # load sim parameters
+    sim_parameters = Parameters(standard_parameters_df)
+
+    # set steps number
+    steps = 300
+
+    # compute initial tumor diameter, knowing that the final correspond to 600 um
+    td_f = sim_parameters.get_value("max_tumor_diameter")  # correspond to 600 um
+    tgr = sim_parameters.get_value("tgr")
+    td_i = td_f / (tgr ** (steps / 3))
+    sim_parameters.set_value("min_tumor_diameter", td_i)  # correspond to d_0
+
+    sim_parameters.set_value("V_uc_af", 21.1608695473508)  # values given in [1 / tau]
+
+    sim_parameters.set_value("V_pT_af", 2.00243118326342)  # values given in [afau / tau]
+
+    run_simulation(spatial_dimension=3,
+                   sim_parameters=sim_parameters,
+                   steps=steps,
+                   save_rate=10,
+                   out_folder_name="Fig4d",
+                   sim_rationale=f"Reproducing simulation in Fig4d",
+                   slurm_job_id=slurm_job_id)
+
+
+def main():
+    tutorial_2d()
 
 
 if __name__ == "__main__":
